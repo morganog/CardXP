fe45097bca9a46717e9934785eebc95bac1d8deb
morganog <morgan.ogborn@gmail.com>
2025-08-05 22:03:22 -0500

    move stylesheet to bottom fixed view, added safeareaview

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,26 @@
 import { useRouter } from "expo-router";
-import * as React from "react";
+import { useState } from "react";
 import { SafeAreaView, StyleSheet } from 'react-native';
 import CardXpHeader from "../components/CardXpHeader";
 import GraderButton from "../components/GraderButton";
 import PreScan from "../components/PreScan";
 import SettingsButton from "../components/SettingsButton";
 import UploadButtons from "../components/UploadButtons";
+
+export default function App() {
+  const router = useRouter();
+  const [image, setImage] = useState(null);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <CardXpHeader />
+      <SettingsButton />
+      <PreScan />
+      <GraderButton />
+      <UploadButtons />
+    </SafeAreaView>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -19,17 +34,3 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   }
 });
-
-export default function App() {
-  const router = useRouter();
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <CardXpHeader />
-      <SettingsButton />
-      <PreScan />
-      <GraderButton />
-      <UploadButtons />
-    </SafeAreaView>
-  );
-}
